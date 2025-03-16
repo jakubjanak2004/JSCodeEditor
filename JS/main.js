6488fa608fc20557a52d0be04bddeb17a45af928
Jakub Janak <janakja5@fel.cvut.cz>
2025-03-16 15:53:45 +0100

    code window js changed into oop

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,7 @@
 import { dragbarFunction } from "./drag/dragbar.js";
 import { leftPanel } from "./left-panel.js";
-import { codeWindow } from "./code-window.js";
 import { dragAndDropFiles } from "./fileSystem/drag-and-drop.js";
 
 leftPanel();
 dragbarFunction();
-codeWindow();
 dragAndDropFiles();
