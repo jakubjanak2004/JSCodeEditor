8efb8ae51d50860541a25cdc6efc3c683dfb8c4c
Jakub Janak <janakja5@fel.cvut.cz>
2025-05-10 16:26:38 +0200

    better quality gifs added

--- assets/allMovToGif.sh
+++ assets/allMovToGif.sh
@@ -1,14 +1,14 @@
 for f in *.mov; do
   name="${f%.mov}"
 
-  # 1. Generate color palette
-  ffmpeg -y -i "$f" -vf "fps=10,scale=480:-1:flags=lanczos,palettegen" "${name}_palette.png"
+  # 1. Generate color palette at full resolution
+  ffmpeg -y -i "$f" -vf "fps=10,palettegen" "${name}_palette.png"
 
-  # 2. Use palette to generate high-quality GIF
+  # 2. Use palette to generate high-quality full-res GIF
   ffmpeg -i "$f" -i "${name}_palette.png" -filter_complex \
-    "fps=10,scale=480:-1:flags=lanczos[x];[x][1:v]paletteuse" \
+    "fps=10[x];[x][1:v]paletteuse" \
     "${name}.gif"
 
-  # Optional: remove temporary palette file
+  # Cleanup
   rm "${name}_palette.png"
 done
