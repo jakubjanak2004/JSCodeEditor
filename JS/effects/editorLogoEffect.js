026f7e2a54671c777dc2779f7f7131534ff014a4
Jakub Janak <janakja5@fel.cvut.cz>
2025-05-19 14:26:16 +0200

    editor logo removed as it didn't contain anything

--- JS/effects/editorLogoEffect.js
+++
@@ -1,4 +0,0 @@
-export default function editorLogoEffect() {
-    const editorLogo = document.getElementById("editor-logo");
-    // todo
-}
--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,5 @@
 import { dragAndDropFiles } from "./fileSystem/DragAndDrop.js";
-import editorLogoEffect from "./effects/EditorLogoEffect.js";
 import internetConnection from "./internet/internetConnection.js";
 
 dragAndDropFiles();
-editorLogoEffect();
 internetConnection();
