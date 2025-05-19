e2ed0bc6f67ca8b4ac1261df5b9a4609776b20cd
Jakub Janak <janakja5@fel.cvut.cz>
2025-05-19 15:09:54 +0200

    refactored the main js file

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
+// this is a main JS file
+// for loading the base functions and executing them
 import { dragAndDropFiles } from "./fileSystem/DragAndDrop.js";
 import internetConnection from "./internet/internetConnection.js";
 
